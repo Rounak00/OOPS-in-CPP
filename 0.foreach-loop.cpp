651392d66de127a7aceccc5e23f4281cdcfece1b
Rounak00 <80785919+Rounak00@users.noreply.github.com>
2021-07-04 09:54:13 +0530

    STL attached

---
+++ 0.foreach-loop.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+// we can do like this also
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[]={1,2,3,4,5};
+
+      
+    for(int i: a){              // it is call for range loop
+        cout<<i<<" ";
+    }
+    return 0;
+}
+
+
+// now see for each loop
+//under algorithm header file
+for_each(vec.begin(), vec.end(), [](int i){
+  cout<<i<<" ";
+});
+
+}
