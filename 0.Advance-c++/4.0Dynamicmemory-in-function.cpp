e59c53f105bcffadd53ddd958b30c3ddf29bd4c1
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-08-22 17:26:34 +0530

    commit

---
+++ 0.Advance-c++/4.0Dynamicmemory-in-function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int *fun()
+{
+    int *ptr = new int;
+    *ptr = 10;
+    return ptr;
+}
+
+int main() 
+{
+    int x, y;
+    cout << *fun();
+    return 0;
+}
